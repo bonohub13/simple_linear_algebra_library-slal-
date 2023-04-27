d5e421d6f428d7b54cb57281052f4de3541a7a1d
Kensuke Saito <ken.saito.0813@gmail.com>
2023-04-27 21:29:53 +0900

    Added documentation for `src/vertex/utils.rs`

--- src/vertex/utils.rs
+++ src/vertex/utils.rs
@@ -41,8 +41,9 @@ macro_rules! vertex_index {
 vertex_index! {i8 u8 i16 u16 i32 u32 i64 u64 i128 u128 isize usize f32 f64}
 
 macro_rules! impl_round {
-    ($($t:ty)*) => ($(
+    ($t:ty, $doc:expr) => {
         impl crate::utils::Round for super::Vertex<$t> {
+            #[doc=$doc]
             fn round(&mut self) {
                 use rayon::prelude::*;
 
@@ -51,7 +52,23 @@ macro_rules! impl_round {
                 });
             }
         }
-    )*)
+    };
+
+    ($t:ty) => {
+        impl_round!(
+            $t,
+            concat!(
+                r"Rounds floating point numbers inside `Vertex::<",
+                stringify!($t),
+                r">`\n\n\n# Examples\n\n```\n\nuse slal::vertex::Vertex;\n\nuse slal::utils::Round;\n\n\nlet mut v = Vertex::<",
+                stringify!($t),
+                r">::new(&[1.1, 2.02, 3.003]);\n\n\nv.round();\n\n\nassert!(v == Vertex::<",
+                stringify!($t),
+                r">::new(&[1., 2., 3.]));\n\n```"
+            )
+        );
+    };
 }
 
-impl_round! {f32 f64}
+impl_round! { f32 }
+impl_round! { f64 }
