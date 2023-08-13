51674bfd4c3fe5026285bbeb0c5b0650cf4f1503
Kensuke Saito <ken.saito.0813@gmail.com>
2023-08-14 00:35:13 +0900

    Added `From` trait implementation for conversion from `&slal::matrix::Matrix` to `slal::matrix::Matrix`

--- src/matrix/convert.rs
+++ src/matrix/convert.rs
@@ -88,3 +88,94 @@ impl_from! { u32, f64 }
 
 // Float -> Float
 impl_from! { f32, f64 }
+
+macro_rules! impl_from_ref {
+    ($Small: ty, $Large: ty, $doc: expr) => {
+        impl From<&super::Matrix<$Small>> for super::Matrix<$Large> {
+            #[doc = $doc]
+            #[inline(always)]
+            fn from(other: &super::Matrix<$Small>) -> Self {
+                use rayon::prelude::*;
+
+                let m: Vec<$Large> = other.m.par_iter().map(|m_ij| *m_ij as $Large).collect();
+
+                super::Matrix::<$Large> {
+                    m,
+                    size: other.size,
+                }
+            }
+        }
+    };
+
+    ($Small: ty, $Large: ty) => {
+        impl_from_ref!(
+            $Small,
+            $Large,
+            concat!(
+                "Converts `&slal::matrix::Matrix<",
+                stringify!($Small),
+                ">` to `&slal::matrix::Matrix<",
+                stringify!($Large),
+                ">` losslessly"
+            )
+        );
+    };
+}
+
+// Unsigned -> Unsigned
+impl_from_ref! { u8, u16 }
+impl_from_ref! { u8, u32 }
+impl_from_ref! { u8, u64 }
+impl_from_ref! { u8, u128 }
+impl_from_ref! { u16, u32 }
+impl_from_ref! { u16, u64 }
+impl_from_ref! { u16, u128 }
+impl_from_ref! { u32, u64 }
+impl_from_ref! { u32, u128 }
+impl_from_ref! { u64, u128 }
+
+// Signed -> Signed
+impl_from_ref! { i8, i16 }
+impl_from_ref! { i8, i32 }
+impl_from_ref! { i8, i64 }
+impl_from_ref! { i8, i128 }
+impl_from_ref! { i16, i32 }
+impl_from_ref! { i16, i64 }
+impl_from_ref! { i16, i128 }
+impl_from_ref! { i32, i64 }
+impl_from_ref! { i32, i128 }
+impl_from_ref! { i64, i128 }
+
+// Unsigned -> Signed
+impl_from_ref! { u8, i16 }
+impl_from_ref! { u8, i32 }
+impl_from_ref! { u8, i64 }
+impl_from_ref! { u8, i128 }
+impl_from_ref! { u16, i32 }
+impl_from_ref! { u16, i64 }
+impl_from_ref! { u16, i128 }
+impl_from_ref! { u32, i64 }
+impl_from_ref! { u32, i128 }
+impl_from_ref! { u64, i128 }
+
+// primitive integer number -> size
+impl_from_ref! { u16, usize }
+impl_from_ref! { u8, usize }
+impl_from_ref! { i16, usize }
+
+// Signed -> Float
+impl_from_ref! { i8, f32 }
+impl_from_ref! { i8, f64 }
+impl_from_ref! { i16, f32 }
+impl_from_ref! { i16, f64 }
+impl_from_ref! { i32, f64 }
+
+// Unsigned -> Float
+impl_from_ref! { u8, f32 }
+impl_from_ref! { u8, f64 }
+impl_from_ref! { u16, f32 }
+impl_from_ref! { u16, f64 }
+impl_from_ref! { u32, f64 }
+
+// Float -> Float
+impl_from_ref! { f32, f64 }
